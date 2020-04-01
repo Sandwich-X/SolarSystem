ad3239eeeec1e73a55ff11d5c7cd1d170786c85a
Sandwich-X <60961092+Sandwich-X@users.noreply.github.com>
2020-04-01 12:59:43 +0200

    Using different split-parameter
    Separation by white-space --> no empty fields

--- graph-viewer.py
+++ graph-viewer.py
@@ -3,34 +3,26 @@
 Viewer for Fortran-datafile
 '''
 
-import PySimpleGUI as sg
+import PySimpleGUI       as sg
 import matplotlib.pyplot as plt
-import numpy as np
-import os          # os.path.join , os.walk
+import numpy             as np
+import os                # os.path.join
 
 SOURCE_FILE = os.path.join("data", "coo.dat")
 
 def create_layout():
     return [
         [sg.Text('N-body data viewer')],
-        [sg.Text("Planeten-Nummer:"),
-         sg.InputText("7", key="bodynr", size=(3,1))],
-        [sg.Text("Number of last lines:"),
-         sg.InputText("7", key="linesnr", size=(3,1))],
-        [sg.Text("x:"),
-         sg.InputText("1", key="xnr", size=(3,1))], # combo !
-        [sg.Text("y:"),
-         sg.InputText("2", key="ynr", size=(3,1))], # combo !
-        [sg.Text("z:"),
-         sg.InputText("0", key="znr", size=(3,1))], # combo !
-        [sg.Ok(),
-         sg.Cancel()]
+        [sg.Text("Planeten-Nummer:"),      sg.InputText( "7", key="bodynr", size=(3,1))],
+        [sg.Text("Number of last lines:"), sg.InputText("10", key="linesnr", size=(3,1))],
+        [sg.Text("x axis:"), sg.InputText("1", key="xnr", size=(3,1))], # combo !
+        [sg.Text("y axis:"), sg.InputText("2", key="ynr", size=(3,1))], # combo !
+        [sg.Text("z axis:"), sg.InputText("0", key="znr", size=(3,1))], # combo !
+        [sg.Ok(), sg.Cancel()]
         ]
 
 layout = create_layout()
 window = sg.Window("Viewer window",location=(100,100)).Layout(layout)
-
-cols = "xyz"
 
 while True:
     event, values = window.read()
@@ -38,38 +30,44 @@ while True:
     if event in [None, "Cancel"]:
         break
     if event == "Ok":
+        body_nr = int(values["bodynr"])
+        x_nr = int(values["xnr"])
+        y_nr = int(values["ynr"])
+        z_nr = int(values["znr"])
+        dim3 = (z_nr != 0) # 3D: 3-DIMensional
+        print("body_nr:", body_nr)
+        print("dim3:   ", dim3)
         data = {"x":[], "y":[], "z":[]}
-        with open(SOURCE_FILE) as f:
-            for number, line in enumerate(f):
+        counter = 0
+        with open(SOURCE_FILE) as sf:
+            for number, line in enumerate(sf):
                 #print("number:", number)
                 #print("line:", line)
-                if number < 7:
+                if number < 7: # 1st 6 lines commentary
                     continue
-                fields = line.split(" ")
-                print("fields:", fields)
-                counter = -1
-                for field in fields:
-                    print("field:", field)
-                    input()
-                    if field == "":
-                        continue
-                    counter += 1
-                    print("counter1:", counter)
-                    input()
-                    if counter == 0: # = body-nr spalte
-                        if int(field) != int(values["bodynr"]):
-                            break # inner for-loop
-                    print("counter2:", counter)
-                    input()
-                    if   counter == int(values["xnr"]):
-                        data["x"].append(float(field))
-                    elif counter == int(values["ynr"]):
-                        data["y"].append(float(field))
-                    elif counter == int(values["znr"]):
-                        data["z"].append(float(field))
+                fields = line.split() # separation by any whitespace
+                #print("fields:", fields)
+                #input()
+                if int(fields[0]) != body_nr:
+                    continue # for-loop -> next number, line
+                counter += 1
+                #print(f"Got {counter}. datapoint:")
+                #print(" x-value:", fields[x_nr])
+                #print(" y-value:", fields[y_nr])
+                data["x"].append(float(fields[x_nr]))
+                data["y"].append(float(fields[y_nr]))
+                if dim3:
+                    data["z"].append(float(fields[z_nr]))
+                    #print(" z-value:", fields[z_nr])
+                #print("len(data['x']):", len(data['x']))
+                #print("len(data['y']):", len(data['y']))
+                #print("len(data['z']):", len(data['z']))
+                #input()
+            #end for
+        #end with
         #input()
-        print("data:", data)
-                    
-                        
+        print("len(data['x']):", len(data['x']))
+        print("len(data['y']):", len(data['y']))
+        print("len(data['z']):", len(data['z']))
 
 print("Bye")
