6fb8e92feaab14b6b4328e914f75a5b3c2b5b722
Sandwich-X <60961092+Sandwich-X@users.noreply.github.com>
2020-04-03 10:16:04 +0200

    simple 2d and 3d possible

--- graph-viewer.py
+++ graph-viewer.py
@@ -5,6 +5,7 @@ Viewer for Fortran-datafile
 
 import PySimpleGUI       as sg
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D # eigentlich ab 3.2.0 nicht nötig
 import numpy             as np
 import os                # os.path.join
 
@@ -14,15 +15,18 @@ def create_layout():
     return [
         [sg.Text('N-body data viewer')],
         [sg.Text("Planeten-Nummer:"),      sg.InputText( "7", key="bodynr", size=(3,1))],
-        [sg.Text("Number of last lines:"), sg.InputText("10", key="linesnr", size=(3,1))],
         [sg.Text("x axis:"), sg.InputText("1", key="xnr", size=(3,1))], # combo !
         [sg.Text("y axis:"), sg.InputText("2", key="ynr", size=(3,1))], # combo !
         [sg.Text("z axis:"), sg.InputText("0", key="znr", size=(3,1))], # combo !
-        [sg.Ok(), sg.Cancel()]
+        [sg.Ok(), sg.Cancel()],
+        [sg.Text("Number of last lines to show:"), sg.InputText("10", key="linesnr", size=(7,1)),
+         sg.Button("Show")],
         ]
 
 layout = create_layout()
-window = sg.Window("Viewer window",location=(100,100)).Layout(layout)
+window = sg.Window("Viewer window",location=(200,200)).Layout(layout)
+
+data = {"x":[], "y":[], "z":[]}
 
 while True:
     event, values = window.read()
@@ -37,8 +41,7 @@ while True:
         dim3 = (z_nr != 0) # 3D: 3-DIMensional
         print("body_nr:", body_nr)
         print("dim3:   ", dim3)
-        data = {"x":[], "y":[], "z":[]}
-        counter = 0
+        #counter = 0
         with open(SOURCE_FILE) as sf:
             for number, line in enumerate(sf):
                 #print("number:", number)
@@ -50,7 +53,7 @@ while True:
                 #input()
                 if int(fields[0]) != body_nr:
                     continue # for-loop -> next number, line
-                counter += 1
+                #counter += 1
                 #print(f"Got {counter}. datapoint:")
                 #print(" x-value:", fields[x_nr])
                 #print(" y-value:", fields[y_nr])
@@ -69,5 +72,20 @@ while True:
         print("len(data['x']):", len(data['x']))
         print("len(data['y']):", len(data['y']))
         print("len(data['z']):", len(data['z']))
-
+    if event == "Show":
+        if len(data['x']) == 0 or len(data['y']) == 0:
+            sg.Popup("No data read!")
+            continue
+        lines_nr = int(values["linesnr"])
+        visual_data = {'x':data["x"][-lines_nr:],
+                       'y':data["y"][-lines_nr:],
+                       'z':data["z"][-lines_nr:]}
+        print("len(visual_data['x']:",len(visual_data['x']))
+        if dim3:
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection='3d')
+            ax.plot(visual_data["x"],visual_data["y"],visual_data["z"], '.')
+        else:
+            plt.plot(visual_data["x"],visual_data["y"])
+        plt.show()
 print("Bye")
