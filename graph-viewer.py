35b4460c81aaf6f5c99057a4c91e762f2db3339b
Sandwich-X <60961092+Sandwich-X@users.noreply.github.com>
2020-04-07 09:29:48 +0200

    Read all data of file

--- graph-viewer.py
+++ graph-viewer.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python
 '''
 Viewer for Fortran-datafile
@@ -14,78 +15,114 @@ SOURCE_FILE = os.path.join("data", "coo.dat")
 def create_layout():
     return [
         [sg.Text('N-body data viewer')],
-        [sg.Text("Planeten-Nummer:"),      sg.InputText( "7", key="bodynr", size=(3,1))],
-        [sg.Text("x axis:"), sg.InputText("1", key="xnr", size=(3,1))], # combo !
-        [sg.Text("y axis:"), sg.InputText("2", key="ynr", size=(3,1))], # combo !
-        [sg.Text("z axis:"), sg.InputText("0", key="znr", size=(3,1))], # combo !
-        [sg.Ok(), sg.Cancel()],
-        [sg.Text("Number of last lines to show:"), sg.InputText("10", key="linesnr", size=(7,1)),
-         sg.Button("Show")],
+        [sg.Frame(" File",
+                  [[sg.Button("Read")]])],
+        [sg.Frame(" Data ",
+                  [[sg.Text("Planeten-Nummer:"), sg.InputText( "7", key="bodynr", size=(3,1))],
+                   [sg.Text("x axis:"), sg.InputText("1", key="xnr", size=(3,1))], # combo !
+                   [sg.Text("y axis:"), sg.InputText("2", key="ynr", size=(3,1))], # combo !
+                   [sg.Text("z axis:"), sg.InputText("0", key="znr", size=(3,1))], # combo !
+                  ])],
+        [sg.Frame(" Plot ",
+                  [[sg.Text("Number of last lines to show:"), sg.InputText("10", key="linesnr", size=(7,1)),
+                    sg.Button("Show")]])],
+        [sg.Cancel()],
         ]
 
 layout = create_layout()
 window = sg.Window("Viewer window",location=(200,200)).Layout(layout)
 
-data = {"x":[], "y":[], "z":[]}
-
+plt.ion()
 while True:
     event, values = window.read()
     print("=== event:", event, "===")
     if event in [None, "Cancel"]:
         break
-    if event == "Ok":
-        body_nr = int(values["bodynr"])
+    if event == "Read":
+        data = {}
+        #data = {"x":[], "y":[], "z":[]}
         x_nr = int(values["xnr"])
         y_nr = int(values["ynr"])
         z_nr = int(values["znr"])
         dim3 = (z_nr != 0) # 3D: 3-DIMensional
-        print("body_nr:", body_nr)
         print("dim3:   ", dim3)
         #counter = 0
         with open(SOURCE_FILE) as sf:
+            #--- 1st: check how many body(number)s and columns
+            #         and create data:
+            for i in range(5): # 0..4 # 5 commentary lines at beginning
+                print("i:", i, sf.readline())
+            nam_cols = sf.readline().split() # NAMe of COLumns
+            print("nam_cols:", nam_cols)
+            line1 = sf.readline().split()
+            num_cols = len(line1)
+            print("line1:", line1)
+            print("num_cols:", num_cols)
+            body1 = line1[0]
+            num_bodies = 1
+            nam_bodies = [body1]
+            print("body1:", body1)
+            data[body1] = [[] for i in range(1,num_cols) ]
+            print("data:", data)
+            while True:
+                body_nr = sf.readline().split()[0]
+                if body_nr == body1:
+                    break # while True
+                data[body_nr] = [[] for i in range(len(line1)-1) ]
+                num_bodies += 1
+                nam_bodies += body_nr
+            print("data:", data)
+            print("num_bodies:", num_bodies)
+            print("nam_bodies:", nam_bodies)
+            input()
+            #--- 2nd: rewind and read all data:
+            sf.seek(0)
             for number, line in enumerate(sf):
                 #print("number:", number)
                 #print("line:", line)
-                if number < 7: # 1st 6 lines commentary
+                #input()
+                if number < 6: # 1st 5 lines commentary
                     continue
                 fields = line.split() # separation by any whitespace
                 #print("fields:", fields)
                 #input()
-                if int(fields[0]) != body_nr:
-                    continue # for-loop -> next number, line
                 #counter += 1
                 #print(f"Got {counter}. datapoint:")
                 #print(" x-value:", fields[x_nr])
                 #print(" y-value:", fields[y_nr])
-                data["x"].append(float(fields[x_nr]))
-                data["y"].append(float(fields[y_nr]))
-                if dim3:
-                    data["z"].append(float(fields[z_nr]))
+                for i in range(1,num_cols):
+                    data[fields[0]][i-1].append(float(fields[i]))
+                #data[body_nr][y_nr-1].append(float(fields[y_nr]))
+                #if dim3:
+                    #data[body_nr][z_nr].append(float(fields[z_nr]))
                     #print(" z-value:", fields[z_nr])
-                #print("len(data['x']):", len(data['x']))
-                #print("len(data['y']):", len(data['y']))
-                #print("len(data['z']):", len(data['z']))
+                #print("data:", data)
                 #input()
             #end for
         #end with
         #input()
-        print("len(data['x']):", len(data['x']))
-        print("len(data['y']):", len(data['y']))
-        print("len(data['z']):", len(data['z']))
+        print("#bodies = len(data)          :", len(data))
+        print("#cols   = len(data[body1])   :", len(data[body1]))
+        print("#data   = len(data[body1][0]):", len(data[body1][0]))
     if event == "Show":
-        if len(data['x']) == 0 or len(data['y']) == 0:
-            sg.Popup("No data read!")
-            continue
+        body_nr = values["bodynr"] # int(%)
+        print("body_nr:", body_nr)
+        #if len(data['x']) == 0 or len(data['y']) == 0:
+        #    sg.Popup("No data read!")
+        #    continue
         lines_nr = int(values["linesnr"])
-        visual_data = {'x':data["x"][-lines_nr:],
-                       'y':data["y"][-lines_nr:],
-                       'z':data["z"][-lines_nr:]}
-        print("len(visual_data['x']:",len(visual_data['x']))
+        #input("-1-")
+        visual_data = {'x':data[body_nr][int(values["xnr"])][-lines_nr:],
+                       'y':data[body_nr][int(values["ynr"])][-lines_nr:],
+                       'z':data[body_nr][int(values["znr"])][-lines_nr:]}
+        #input("-2-")
+        #print("len(visual_data['x']:",len(visual_data['x']))
         if dim3:
             fig = plt.figure()
             ax = fig.add_subplot(111, projection='3d')
             ax.plot(visual_data["x"],visual_data["y"],visual_data["z"], '.')
         else:
             plt.plot(visual_data["x"],visual_data["y"])
+        #input("-3-")
         plt.show()
 print("Bye")
